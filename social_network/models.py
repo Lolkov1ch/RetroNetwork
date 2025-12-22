48ef32ae43992d3a833f2e93721e6746909fc12c
Lolkov1ch <radioactive1161@gmail.com>
2025-12-22 22:24:10 +0200

    added comments, buttons for creating/deleting/editing posts and etc.

--- social_network/models.py
+++ social_network/models.py
@@ -36,15 +36,22 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"Comment by {self.author} on Post #{self.post.id}"
+
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="likes", null=True, blank=True)
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name="likes"
     )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} likes Post #{self.post.id}"
 
     class Meta:
         unique_together = ("post", "user")
--- social_network/views.py
+++ social_network/views.py
@@ -1,14 +1,111 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, RedirectView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from social_network.models import Post, Comment, Like
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404
+from django.views import View
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "social_network/post_form.html"
+    context_object_name = "post"
+    fields = ["content"]
+    success_url = reverse_lazy("social_network:post_base")
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 class PostListView(ListView):
     model = Post
-    template_name = "social_network/post_list.html"
+    template_name = "social_network/post_base.html"
     context_object_name = "posts"
     paginate_by = 10
     
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "social_network/post_detail.html"    
+    class PostLikeView(LoginRequiredMixin, View):
+        def post(self, request, pk):
+            post = get_object_or_404(Post, pk=pk)
 
+            like, created = Like.objects.get_or_create(
+                post=post,
+                user=request.user
+            )
+
+            if not created:
+                like.delete()
+                messages.info(request, "Like removed.")
+            else:
+                messages.success(request, "Post liked!")
+
+            return redirect("social_network:post_detail", pk=post.pk)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = "social_network/post_form.html"
+    context_object_name = "post"
+    fields = ["content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("social_network:post_detail", kwargs={"pk": self.object.pk})
+    
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = "social_network/post_delete.html"
+    context_object_name = "post"
+    success_url = reverse_lazy("social_network:post_base")
+    
+
+class PostLikeView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+
+        like, created = Like.objects.get_or_create(
+            post=post,
+            user=request.user
+        )
+
+        if not created:
+            like.delete()
+            messages.info(request, "Like removed.")
+        else:
+            messages.success(request, "Post liked!")
+
+        return redirect("social_network:post_detail", pk=post.pk)
+
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    template_name = "social_network/comment_form.html"
+    context_object_name = "comment"
+    fields = ["content"]
+
+    def form_valid(self, form):
+        post_pk = self.kwargs.get("pk")
+        post = get_object_or_404(Post, pk=post_pk)
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post_pk = self.kwargs.get("pk")
+        context['post'] = get_object_or_404(Post, pk=post_pk)
+        return context
+
+    def get_success_url(self):
+        post_pk = self.kwargs.get("pk")
+        return reverse("social_network:post_detail", kwargs={"pk": post_pk})
